edfd7945df83a341fd893f140a311d134ec30089
Qusai-netizen <qxmlight@gmail.com>
2026-05-26 23:07:44 +0300

    Add files via upload

--- statistics/mainN.java
+++ statistics/mainN.java
@@ -1,114 +1,97 @@
-package statistics;
+package app;
 
-import statistics.UI.ChartsPanel;
-import statistics.UI.TablesPanel;
-import statistics.UI.ui;
-import statistics.UIData.ClassFrequencyDataset;
-import statistics.UIData.NumericSamplesDataset;
-import statistics.UIData.StringSamplesDataset;
-import statistics.UIData.Table;
+import java.util.ArrayList;
+
+import UI.ChartsPanel;
+import UI.StatisPanel;
+import UI.TablesPanel;
+import UI.ui;
+import UIData.ClassFrequencyDataset;
+import UIData.NumericSamplesDataset;
+import UIData.StringSamplesDataset;
+import UIData.Table;
+import storage.statisDb;
+import storage.statisDb.Experiment;
 
 public class mainN {
 
+    public static statisDb db;
+
     public static void main(String[] args) {
-        String type = "fc";
-        int classNum = 5;
+        new ui();
+    }
 
-        if (type == "s") {
+    public static StatisPanel buildStatisPanel(Experiment experiment) {
+        return buildStatisPanel(experiment, null);
+    }
 
-            String[] samplesS = new String[] { "A", "B", "A", "D", "B" };
-
-            StringSamplesDataset dataset = new StringSamplesDataset(samplesS);
-
-            Table t = new Table(dataset.getMainTable(1), dataset.getMainColumns(1));
-
-            new ui(t.scrollPane);
+    public static StatisPanel buildStatisPanel(Experiment experiment, ui mainFrame) {
+        if (experiment == null) {
+            throw new IllegalArgumentException("Experiment cannot be null.");
         }
 
-        // Fill samples
-        else if (type == "n") {
-
-            // Sample data
-            int[] Samples = new int[] {
-                    62, 72, 66, 79, 83, 61, 62, 95, 72, 64, 74, 71, 42, 38, 91, 66, 77, 90, 74, 63, 64, 68, 42
-            };
-
-            // Create calculator
-            NumericSamplesDataset dataset = new NumericSamplesDataset(Samples, classNum);
-
-            // Generate statistics
+        if (experiment.type == Experiment.enType.QUAL) {
+            StringSamplesDataset dataset = new StringSamplesDataset(experiment.samplesS);
             dataset.build();
 
-            // Combine tables data
-            Object[][][] tablesData = new Object[2][][];
-            tablesData[0] = dataset.getMainTable(1);
-            tablesData[1] = dataset.getMainTable(2);
+            Table table = new Table(dataset.getMainTable(1), dataset.getMainColumns(1));
+            return new StatisPanel(table.scrollPane, mainFrame);
+        }
 
-            // Combine tables columns
-            String[][] tabesColumns = new String[2][];
-            tabesColumns[0] = dataset.getMainColumns(1);
-            tabesColumns[1] = dataset.getMainColumns(2);
+        if (experiment.type == Experiment.enType.QUAN) {
+            NumericSamplesDataset dataset = new NumericSamplesDataset(experiment.samplesInt, experiment.getClassesNum());
+            dataset.build();
 
-            TablesPanel tP = new TablesPanel(tablesData, tabesColumns);
+            ArrayList<ArrayList<ArrayList<Object>>> tablesData = new ArrayList<>();
+            tablesData.add(dataset.getMainTable(1));
+            tablesData.add(dataset.getMainTable(2));
 
-            double[] boundaries = new double[dataset.calc.quan.classBoundlower.length + 1];
-            for (int i = 0; i < dataset.calc.quan.classBoundlower.length; i++) {
-                boundaries[i] = dataset.calc.quan.classBoundlower[i];
-            }
-            boundaries[boundaries.length
-                    - 1] = dataset.calc.quan.classBoundupper[dataset.calc.quan.classBoundupper.length - 1];
+            ArrayList<ArrayList<String>> tablesColumns = new ArrayList<>();
+            tablesColumns.add(dataset.getMainColumns(1));
+            tablesColumns.add(dataset.getMainColumns(2));
 
-            ChartsPanel charts = new ChartsPanel(
+            TablesPanel tablesPanel = new TablesPanel(tablesData, tablesColumns);
+            ChartsPanel chartsPanel = new ChartsPanel(
                     dataset.calc.quan.classRelativeF,
                     dataset.calc.quan.freq,
                     dataset.calc.quan.midPoint,
                     dataset.calc.quan.classBoundariesLowUp,
                     dataset.calc.quan.ascendingComulativeFreq,
-                    boundaries);
+                    buildBoundaries(dataset.calc.quan.classBoundlower, dataset.calc.quan.classBoundupper));
 
-            new ui(tP, charts);
+            return new StatisPanel(tablesPanel, chartsPanel, mainFrame);
         }
 
-        else if (type == "fc") {
+        ClassFrequencyDataset dataset = new ClassFrequencyDataset(experiment.classes, experiment.freq);
+        dataset.build();
 
-            // Sample data
-            String[] _class = { "80 - 109", "110 - 139", "140 - 169", "170 - 199", "200 - 229", "230 - 259" };
-            int[] freq = { 26, 78, 122, 34, 14, 8 };
+        ArrayList<ArrayList<ArrayList<Object>>> tablesData = new ArrayList<>();
+        tablesData.add(dataset.getMainTable(1));
+        tablesData.add(dataset.getMainTable(2));
 
-            // Create calculator
-            ClassFrequencyDataset dataset = new ClassFrequencyDataset(_class, freq);
+        ArrayList<ArrayList<String>> tablesColumns = new ArrayList<>();
+        tablesColumns.add(dataset.getMainColumns(1));
+        tablesColumns.add(dataset.getMainColumns(2));
 
-            // Generate statistics
-            dataset.build();
+        TablesPanel tablesPanel = new TablesPanel(tablesData, tablesColumns);
+        ChartsPanel chartsPanel = new ChartsPanel(
+                dataset.calc.quanC.classRelativeF,
+                dataset.calc.quanC.freq,
+                dataset.calc.quanC.midPoint,
+                dataset.calc.quanC.classBoundariesLowUp,
+                dataset.calc.quanC.ascendingComulativeFreq,
+                buildBoundaries(dataset.calc.quanC.classBoundlower, dataset.calc.quanC.classBoundupper));
 
-            // Combine tables data
-            Object[][][] tablesData = new Object[2][][];
-            tablesData[0] = dataset.getMainTable(1);
-            tablesData[1] = dataset.getMainTable(2);
+        return new StatisPanel(tablesPanel, chartsPanel, mainFrame);
+    }
 
-            // Combine tables columns
-            String[][] tabesColumns = new String[2][];
-            tabesColumns[0] = dataset.getMainColumns(1);
-            tabesColumns[1] = dataset.getMainColumns(2);
+    private static ArrayList<Double> buildBoundaries(ArrayList<Double> lowerBounds, ArrayList<Double> upperBounds) {
+        ArrayList<Double> boundaries = new ArrayList<>(lowerBounds);
 
-            TablesPanel tP = new TablesPanel(tablesData, tabesColumns);
-
-            double[] boundaries = new double[dataset.calc.quanC.classBoundlower.length + 1];
-            for (int i = 0; i < dataset.calc.quanC.classBoundlower.length; i++) {
-                boundaries[i] = dataset.calc.quanC.classBoundlower[i];
-            }
-            boundaries[boundaries.length
-                    - 1] = dataset.calc.quanC.classBoundupper[dataset.calc.quanC.classBoundupper.length - 1];
-
-            ChartsPanel charts = new ChartsPanel(
-                    dataset.calc.quanC.classRelativeF,
-                    dataset.calc.quanC.freq,
-                    dataset.calc.quanC.midPoint,
-                    dataset.calc.quanC.classBoundariesLowUp,
-                    dataset.calc.quanC.ascendingComulativeFreq,
-                    boundaries);
-
-            new ui(tP, charts);
+        if (!upperBounds.isEmpty()) {
+            boundaries.add(upperBounds.get(upperBounds.size() - 1));
         }
+
+        return boundaries;
     }
 }
